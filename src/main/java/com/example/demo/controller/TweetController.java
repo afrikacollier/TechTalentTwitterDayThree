f580fba292f85e3d9d24c56d1233ba9185b53184
afrikacollier <afrikakcfc@gmail.com>
2022-06-21 12:15:38 -0700

    initial commit and some hastag and tweetlist updates

--- src/main/java/com/example/demo/controller/TweetController.java
+++ src/main/java/com/example/demo/controller/TweetController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.models.Tweet;
@@ -29,6 +30,14 @@ public class TweetController {
 		List<Tweet> tweets = tweetService.findAll();
 		model.addAttribute("tweetList", tweets);
 		return "feed";
+	}
+	
+	@GetMapping(value= {"/tweets/{tag}"})
+	public String getTweetsByTag(@PathVariable(value="tag") String tag, Model model) {
+		List<Tweet> tweets = tweetService.findAllWithTag(tag);
+		model.addAttribute("tweetList", tweets);
+		model.addAttribute("tag", tag);
+		return "tagTweets";
 	}
 	
 	@GetMapping(value="/tweets/new")
--- src/main/java/com/example/demo/repository/TweetRepository.java
+++ src/main/java/com/example/demo/repository/TweetRepository.java
@@ -13,4 +13,5 @@ public interface TweetRepository extends CrudRepository<Tweet, Long> {
 	List<Tweet> findAllByOrderByCreatedAtDesc();
 	List<Tweet> findAllByUserOrderByCreatedAtDesc(UserProfile user);
 	List<Tweet> findAllByUserInOrderByCreatedAtDesc(List<UserProfile> users);
+	List<Tweet> findByTags_PhraseOrderByCreatedAtDesc(String phrase);
 }
